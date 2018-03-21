2668a5550710ad1ed69ed9382159c5da5d977f91
Quang Van <quang@absentsoft.com>
2018-03-21 02:29:45 -0400

    remove old routes code

--- src/cljs/my_app/routes.cljs
+++ src/cljs/my_app/routes.cljs
@@ -20,35 +20,3 @@
   (pushy/start! (pushy/pushy dispatch-route parse-url)))
 
 (def url-for (partial bidi/path-for routes))
-
-(comment ns my-app.routes
-  (:require-macros [secretary.core :refer [defroute]])
-  (:import goog.History)
-  (:require [secretary.core :as secretary]
-            [goog.events :as gevents]
-            [goog.history.EventType :as EventType]
-            [re-frame.core :as re-frame]
-            [my-app.events :as events]
-            ))
-
-(comment defn hook-browser-navigation! []
-  (doto (History.)
-    (gevents/listen
-     EventType/NAVIGATE
-     (fn [event]
-       (secretary/dispatch! (.-token event))))
-    (.setEnabled true)))
-
-(comment defn app-routes []
-  (secretary/set-config! :prefix "#")
-  ;; --------------------
-  ;; define routes here
-  (defroute "/" []
-    (re-frame/dispatch [::events/set-active-panel :home-panel]))
-
-  (defroute "/about" []
-    (re-frame/dispatch [::events/set-active-panel :about-panel]))
-
-
-  ;; --------------------
-  (hook-browser-navigation!))
